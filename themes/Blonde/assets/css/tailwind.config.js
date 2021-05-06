be963c2d4cea7b0f80960eb17cedf3336629a3af
martinsitar <martin.sitar22@gmail.com>
2021-05-05 20:26:55 -0400

    fonts, nav

--- themes/Blonde/assets/css/tailwind.config.js
+++ themes/Blonde/assets/css/tailwind.config.js
@@ -1,17 +1,14 @@
 module.exports = {
   theme: {
-    extend: {}
-  },
-  theme: {
-    container: {
-      center: true,
-    },
-  },
-  /* darkMode: 'media', */
-  theme: {
     height: {
      '450': '450px',
-    }
+    },
+    extend: {
+      fontFamily: {
+        'overpass': ['overpass', 'sans-serif'],
+        'oswald': ['oswald', 'sans-serif']
+      },
+    },
   },
   variants: {},
   plugins: [
